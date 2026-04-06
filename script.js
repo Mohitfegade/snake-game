455e1949ad2f2bf37187599d345f36776d3aecf7
Mohitfegade <mohitfegade777@gmail.com>
2026-04-06 18:08:26 +0530

    fix the position

--- script.js
+++ script.js
@@ -144,7 +144,7 @@ backgroundMusic.volume = 0.01;
 backgroundMusic.play();
 
 modal.style.display = "none";
-snake = [{x:1, y:2}]   
+snake = [{x:2, y:1}]   
 food = {x: Math.floor(Math.random()*rows), y: Math.floor(Math.random()*cols)};
  intervalID = setInterval(()=>{
         render();
